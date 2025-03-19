bdc816e80bf3922534f34424e654d3c7567a719f
Dave <newdevdave@gmail.com>
2025-03-19 11:36:51 -0400

    yay

--- src/main/java/net/tryhardfilter/Tryhardfilter.java
+++ src/main/java/net/tryhardfilter/Tryhardfilter.java
@@ -1,9 +1,17 @@
 package net.tryhardfilter;
 
+//for the logger
 import net.fabricmc.api.ModInitializer;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+//for the saycmd
+import com.mojang.brigadier.arguments.StringArgumentType;
+import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.client.command.v2.ClientCommandManager;
+import net.fabricmc.fabric.api.client.command.v2.ClientCommandRegistrationCallback;
+import net.minecraft.client.MinecraftClient;
 
 public class Tryhardfilter implements ModInitializer {
 	public static final String MOD_ID = "tryhardfilter";
@@ -11,7 +19,17 @@ public class Tryhardfilter implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
-		LOGGER.info("wassup world");
+		LOGGER.info("^_____^ wassup world");
 
+
+		ClientCommandRegistrationCallback.EVENT.register((dispatcher, registryAccess) -> dispatcher.register(ClientCommandManager.literal("t")
+				.then(ClientCommandManager.argument("message", StringArgumentType.greedyString())
+						.executes(context -> {
+							String message = StringArgumentType.getString(context, "message");
+							String modifiedMessage = message.replaceAll("(?i)ryan", "tryhard");
+
+							MinecraftClient.getInstance().getNetworkHandler().sendChatMessage(modifiedMessage);
+							return 1;
+						}))));
 	}
 }
